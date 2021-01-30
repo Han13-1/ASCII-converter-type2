080fd5fffe271cb568c38902b1c6a58442535f4a
Han13-1 <74622232+Han13-1@users.noreply.github.com>
2021-01-30 20:25:55 +0100

    Update ASCII-type2.py

--- ASCII-type2.py
+++ ASCII-type2.py
@@ -1,8 +1,10 @@
 i=0
 while i< 1:
     x = input("insert your value: ")
+#get ASCII    
     if x.isalpha():
         print("your character is: ",ord(x))
+#differencial        
     elif int(x) in range(0,9):
         n = input("is this an ASCII value? yes or no:  ")
         if n == "yes":
@@ -10,6 +12,11 @@ while i< 1:
         elif n == "no":
             y= int (x)
             print(f'the ASCII value of {y} is: ',chr(y))
-    elif int(x) in range(10,110000):
+#deliver ASCII            
+    elif int(x) in range(10,109999):
         y = int(x)
-        print(f'the ASCII value of {y} is: ',chr(y))
+        print(f'the ASCII value of {y} is:',chr(y))
+#unexistant
+    elif int(x) in range(110000,99999999999999999999999999999999999999):
+        print("anything x>=109999 hasn\'t got an assigned value")
+#(couldn't find how to make +infinity)
